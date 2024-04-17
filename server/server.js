d9ef8ba1659ac1d4c0423f4e5d17ada22d9a2eeb
Samson <reytris666@gmail.com>
2024-04-17 16:30:42 +0300

    обновление сервисов на вервере

--- server/server.js
+++ server/server.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
-// import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
 
 const app = express();
 const PORT = 3000;
 
-// app.use(bodyParser.json());
 app.use(
 	cors({
 		credentials: true,
@@ -62,6 +60,17 @@ app.get('/teams/matches/:id/:dateFrom/:dateTo', async (req, res) => {
 	try {
 		const { data } = await $api.get(
 			`/teams/${id}/matches?dateFrom=${dateFrom}&dateTo=${dateTo}`
+		);
+		res.json(data);
+	} catch (error) {
+		res.status(500).json({ error: 'Internal server error' });
+	}
+});
+app.get('/competitions/matches/:id/:dateFrom/:dateTo', async (req, res) => {
+	const { id, dateFrom, dateTo } = req.params;
+	try {
+		const { data } = await $api.get(
+			`/competitions/${id}/matches?dateFrom=${dateFrom}&dateTo=${dateTo}`
 		);
 		res.json(data);
 	} catch (error) {
